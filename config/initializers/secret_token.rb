62e7d60af3954923ed4fdd60000ab9c52152b242
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-21 21:50:48 +0000

    begin developing Query app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Query::Application.config.secret_key_base = '9dfa89638d252ff0ed8ea292da40e092e028d59d131e3007bf39a2583e07f3119b5a535d03f5b8c4b88520ee10c145fe3e902aaacc042d05c1200fb539190dfd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Query::Application.config.session_store :cookie_store, key: '_query_session'
