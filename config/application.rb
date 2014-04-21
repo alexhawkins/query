10d370e796bac5b2dbbccfc31745af7cb41778f3
Alex Hawkins <alexhawkins.me@gmail.com>
2014-04-21 15:07:22 -0700

    Update Gemfile for Heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Query
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # This change will keep heroku from trying to access the database while it compiles assets.
+    config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
